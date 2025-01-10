4cd0f8f1b9e771adde8413b4e1b62b2e8ae8db78
minhdocs <cristiando0902@gmail.com>
2025-01-10 15:17:14 +0700

    update node for root

--- programs/locker/src/instructions/v3/claim.rs
+++ programs/locker/src/instructions/v3/claim.rs
@@ -38,6 +38,8 @@ impl ClaimV3Params {
             &self.cliff_unlock_amount.to_le_bytes(),
             &self.amount_per_period.to_le_bytes(),
             &self.number_of_period.to_le_bytes(),
+            &self.cliff_time.to_le_bytes(),
+            &self.frequency.to_be_bytes()
         ]);
 
         let leaf = hashv(&[LEAF_PREFIX, &node.to_bytes()]);
