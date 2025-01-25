145896ff5271fbc286159ee1be7ac396881aa470
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2025-01-25 14:44:03 +0700

    reduce test size

--- commands/generate_dummy_lock.sh
+++ commands/generate_dummy_lock.sh
@@ -1,4 +1,4 @@
 
 csv_path=/Users/andrewnguyen/Documents/solana/jup-lock/commands/trash/test.csv
-num_node=1800
+num_node=10
 target/debug/cli generate-test-lock --csv-path $csv_path --num-node $num_node
