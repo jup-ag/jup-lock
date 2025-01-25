d07d2b12bb3a71144c96209bed6e3bf531b70c57
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2025-01-25 14:32:53 +0700

    push samples

--- commands/generate_dummy_lock.sh
+++ commands/generate_dummy_lock.sh
@@ -1,4 +1,4 @@
 
 csv_path=/Users/andrewnguyen/Documents/solana/jup-lock/commands/trash/test.csv
-num_node=18000
+num_node=1800
 target/debug/cli generate-test-lock --csv-path $csv_path --num-node $num_node
