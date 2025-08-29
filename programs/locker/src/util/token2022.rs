5cf106b03b21b778b27e177f3f992a84d463c522
urieltan <urieltan3@gmail.com>
2025-08-29 15:46:15 +0800

    fix calculate pre fee amount

--- programs/locker/src/util/token2022.rs
+++ programs/locker/src/util/token2022.rs
@@ -378,7 +378,7 @@ pub fn calculate_pre_fee_amount(transfer_fee: &TransferFee, post_fee_amount: u64
         let denominator = ONE_IN_BASIS_POINTS.checked_sub(transfer_fee_basis_points)?;
         // let raw_pre_fee_amount = ceil_div(numerator, denominator)?;
         let raw_pre_fee_amount = numerator
-            .checked_add(ONE_IN_BASIS_POINTS)?
+            .checked_add(denominator)?
             .checked_sub(1)?
             .checked_div(denominator)?;
 
